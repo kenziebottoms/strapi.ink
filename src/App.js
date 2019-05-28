4d47e1676dcbf81cd602a2b5346be3e9cc936172
Kenzie Bottoms <kenziebottoms@gmail.com>
2019-05-28 14:42:22 -0500

    use literal svg in logo element; move routes into sidebar component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import Sidebar from './components/Sidebar';
 import Content from './components/Content';
 
 import './styles/App.scss';
 
-const routes = [
-  {
-    name: 'Home',
-    slug: 'home',
-    path: '/home',
-    icon: false,
-  },
-  {
-    name: 'Art',
-    slug: 'art',
-    path: '/art',
-    icon: 'color_lens',
-  },
-  {
-    name: 'Code',
-    slug: 'code',
-    path: '/code',
-    icon: 'code',
-  },
-];
-
 function App() {
   return (
-    <React.Fragment>
-      <Sidebar routes={routes} />
+    <Fragment>
+      <Sidebar />
       <Content />
-    </React.Fragment>
+    </Fragment>
   );
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,14 +6,50 @@ import Logo from '../elements/Logo';
 import '../styles/Sidebar.scss';
 
 class Sidebar extends Component {
+
+  routes() {
+    return [
+      {
+        name: 'home',
+        path: '/',
+        icon: false,
+      },
+      {
+        name: 'art',
+        path: '/art',
+        icon: 'color_lens',
+      },
+      {
+        name: 'code',
+        path: '/code',
+        icon: 'code',
+      },
+      {
+        name: 'resume',
+        path: '/resume',
+        icon: 'assignment_ind',
+      },
+      {
+        name: 'feats',
+        path: '/achievements',
+        icon: 'stars',
+      },
+      {
+        name: 'links',
+        path: '/links',
+        icon: 'launch',
+      },
+    ]
+  };
+
   render() {
     return (
       <ul id="sidebar">
-        {this.props.routes.map(route => (
-          <li key={route.slug}>
+        {this.routes().map(route => (
+          <li key={route.name}>
             <Link to={route.path}>
               {route.icon ? (
-                <i class="material-icons">{route.icon}</i>
+                <i className="material-icons">{route.icon}</i>
               ) : (
                 <Logo link={false} />
               )}
