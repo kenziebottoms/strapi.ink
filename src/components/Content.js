7311f86837bf39ba2748be7eaaaa23c7b1d9c593
Kenzie Bottoms <kenziebottoms@gmail.com>
2019-05-28 14:45:53 -0500

    add switch statement to content and homepage component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,18 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+
+import { Switch, Route } from 'react-router-dom';
+
+import Home from '../pages/Home';
 
 class Content extends Component {
   render() {
-    return <section id="content" />;
+    return (
+      <section id="content">
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </section>
+    );
   }
 }
 
