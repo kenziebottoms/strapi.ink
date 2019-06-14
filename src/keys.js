ca6bbe5d1089a565726ffc366cc6ed955682974b
Kenzie Bottoms <kenziebottoms@gmail.com>
2019-06-14 14:14:34 -0500

    fix key exports

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,4 @@
 export default {
-  RESTDB: {
-    url: 'https://kenziebottoms-c396.restdb.io/rest',
-    key: '5a946ab3696ea8f017645933',
-  },
+  url: 'https://kenziebottoms-c396.restdb.io/rest',
+  key: '5a946ab3696ea8f017645933',
 };
