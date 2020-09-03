bcb7704a8f4e8f413782c707ad398d2eeed92ca6
McKenzie Bottoms <mckenziebottoms@MacBook-Pro.local>
2020-09-03 17:08:40 -0500

    update eslint config, make sidebar a fnal component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,70 +5,67 @@ import Logo from '../elements/Logo'
 
 import '../styles/components/Sidebar.scss'
 
-class Sidebar extends React.Component {
-  routes() {
-    return [
-      {
-        name: 'home',
-        path: '/',
-        icon: false
-      },
-      {
-        name: 'art',
-        path: '/art',
-        icon: 'color_lens'
-      },
-      {
-        name: 'code',
-        path: '/code',
-        icon: 'code'
-      },
-      {
-        name: 'resume',
-        path: '/resume',
-        icon: 'assignment_ind'
-      },
-      {
-        name: 'feats',
-        path: '/achievements',
-        icon: 'stars'
-      },
-      {
-        name: 'links',
-        path: '/links',
-        icon: 'launch'
-      }
-    ]
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    icon: false
+  },
+  {
+    name: 'art',
+    path: '/art',
+    icon: 'color_lens'
+  },
+  {
+    name: 'code',
+    path: '/code',
+    icon: 'code'
+  },
+  {
+    name: 'resume',
+    path: '/resume',
+    icon: 'assignment_ind'
+  },
+  {
+    name: 'feats',
+    path: '/achievements',
+    icon: 'stars'
+  },
+  {
+    name: 'links',
+    path: '/links',
+    icon: 'launch'
   }
+]
 
-  isActive(route) {
-    if (route.icon) {
-      return this.props.location.pathname.indexOf(route.path) >= 0
+const Sidebar = ({ location }) => {
+  const isActive = ({ icon, path }) =>{
+    if (icon) {
+      return location.pathname.indexOf(path) >= 0
     } else {
-      return this.props.location.pathname === '/' 
+      return location.pathname === '/' 
     }
   }
 
-  render() {
-    return (
-      <ul id='sidebar'>
-        {this.routes().map(route => 
-          <li key={route.name}>
-            <Link
-              to={route.path}
-              className={this.isActive(route) ? 'active' : '' }
-            >
-              {route.icon ? 
-                <i className='material-icons'>{route.icon}</i>
-                : 
-                <Logo link={false} />
-              }
-              <label>{route.name}</label>
-            </Link>
-          </li>
-        )}
-      </ul>
-    )
-  }
+  return (
+    <ul id='sidebar'>
+      {routes.map(route => 
+        <li key={route.name}>
+          <Link
+            to={route.path}
+            className={isActive(route) ? 'active' : '' }
+          >
+            {route.icon ? 
+              <i className='material-icons'>{route.icon}</i>
+              : 
+              <Logo link={false} />
+            }
+            <label>{route.name}</label>
+          </Link>
+        </li>
+      )}
+    </ul>
+  )
 }
+
 export default withRouter(Sidebar)
